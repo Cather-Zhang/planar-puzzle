e8f5e254e3087c6d4c69e4de971fae05c8e95bab
Cather-Zhang <cather0215@gmail.com>
2022-09-13 17:19:07 -0400

    added initial gui and class details

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {layout} from './Layout.js';
 
 function App() {
+  const appRef = React.useRef(null);
+  const canvasRef = React.useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={layout.Appmain} ref={appRef}>
+      <canvas tableIndex = "1"
+      className="App-canvas"
+      ref={canvasRef}
+      width={layout.canvas.width}
+      height={layout.canvas.height}
+      />
+
+      <label style={layout.text}>message</label>
+      <div style={layout.buttons}>
+
+      <button style={layout.upbutton}>^</button>
+
+      </div>
+
+    </main>
   );
 }
+
 
 export default App;
---
+++ src/Layout.js
@@ -0,0 +1,28 @@
+export const layout = {
+    canvas: {
+        height: "500",
+        width: "500",
+    },
+    
+    Appmain: {
+        backgroundColor: "#320453",
+        height: "100vh",
+        width: "100vw",
+        
+    },
+
+    buttons: {
+        position: "absolute",
+        left: 250,
+        top: 90
+    },
+
+    text: {
+        position: "absolute",
+        left: 300,
+        top: 100,
+        color: "yellow",
+        backgroundColor: "#320453",
+        width: 80,
+    }
+}
