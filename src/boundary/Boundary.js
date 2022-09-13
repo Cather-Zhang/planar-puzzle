9f48c90c2fe8c10e907479634f4dfc581eded5b6
Cather-Zhang <cather0215@gmail.com>
2022-09-13 18:25:12 -0400

    Update Boundary.js

---
+++ src/boundary/Boundary.js
@@ -0,0 +1,6 @@
+export function redrawCanvas(model, canvasObj, appObj) {
+    const ctx = canvasObj.getContext('2d');
+
+    ctx.clearRect(0,0,canvasObj.width, canvasObj.height)
+    let m = model
+}
