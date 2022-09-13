fda07d5f3714f0a64d447f364434f1de94140cdd
Cather-Zhang <cather0215@gmail.com>
2022-09-13 17:42:06 -0400

    Update Model.js

--- src/model/Model.js
+++ src/model/Model.js
@@ -1,4 +1,18 @@
-export  class Square {
+export class Coordinate {
+    constructor(row, column) {
+        this.row = row;
+        this.column = column;
+    }
+}
+
+export class MoveType {
+    constructor(deltar, deltac) {
+        this.deltar = deltar;
+        this.deltac = deltac;
+    }
+}
+
+export class Square {
     constructor(row, column, color) {
         this.row = row;
         this.column = column;
@@ -16,5 +30,16 @@ export class Puzzle {
 }
 
 export default class Model {
+    constructor(info) {
+        this.initialize(info);
+    }
 
+    initialize(info) {
+        let numRows = parseInt(info.board.rows);
+        let numCols = parseInt(info.board.cols);
+
+        this.puzzle = new Puzzle(numRows, numCols);
+        this.victory = false;
+        this.level = parseInt(info.board.level);
+    }
 }
