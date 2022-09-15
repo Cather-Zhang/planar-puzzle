d28680bc1fa64b89b7e49b62b60c86171eaa717c
Cather Zhang <cather0215@gmail.com>
2022-09-14 21:28:10 -0400

    added initial config

---
+++ src/model/Puzzle.js
@@ -0,0 +1,38 @@
+const puzzleInformation = 
+{
+    "name": "Level 1 puzzle 2x4",
+    "board": {
+        "rows": "2",
+        "cols": "4",
+        "level": "1"
+    },
+    "bases": [
+        {
+            "label": "A",
+            "row": "1",
+            "col": "1",
+            "color": "red"
+        },
+        {
+            "label": "B",
+            "row": "1",
+            "col": "3",
+            "color": "red"
+        },
+        {
+            "label": "C",
+            "row": "1",
+            "col": "4",
+            "color": "orange"
+        },
+        {
+            "label": "D",
+            "row": "2",
+            "col": "3",
+            "color": "orange"
+        }
+
+    ]
+
+
+};
